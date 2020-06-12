0cd8c76cb6c9048c652fecbe95747bbd74024863
Subhasis Das <subhasisdas125@gmail.com>
2020-06-12 22:05:42 +0530

    handleChange and handleSubmit layout ready

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -7,25 +7,51 @@ const Contact = () => {
     subject: "",
     message: "",
   });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    alert("This feature is not yet complete,please comeback later!");
+  };
+
   return (
     <div id="contactForm">
       <legend className="greet">Contact Me</legend>
       <Container id="formContainer">
-        <Form className="text-center">
+        <Form className="text-center" onSubmit={(e) => handleSubmit(e)}>
           <Form.Group className="text-left">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="abc@xyz.com" />
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="abc@xyz.com"
+              value={formData.email}
+              onChange={(e) => handleChange(e)}
+            />
           </Form.Group>
           <Form.Group className="text-left">
             <Form.Label>Subject</Form.Label>
-            <Form.Control type="text" placeholder="Enter subject" />
+            <Form.Control
+              type="text"
+              name="subject"
+              placeholder="Enter subject"
+              value={formData.subject}
+              onChange={(e) => handleChange(e)}
+            />
           </Form.Group>
           <Form.Group className="text-left">
             <Form.Label>Message</Form.Label>
             <Form.Control
               as="textarea"
+              name="message"
               rows="5"
               placeholder="Leave a message for me,I will get back to you at the earliest!"
+              value={formData.message}
+              onChange={(e) => handleChange(e)}
             />
           </Form.Group>
           <Button variant="primary" type="submit">
