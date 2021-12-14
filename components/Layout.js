83c067948fc4a5733fcf47acac131e5a7d489e82
subhasis020299 <subhasisdas125@gmail.com>
2021-12-14 21:15:14 +0530

    fixed layout issues

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Head>
-        <title>Subhasis Das|Portfolio</title>
-        <meta
-          name="description"
-          content="The official portfolio website of Subhasis Das"
-        />
-        <link rel="icon" href="/images/favicon.ico" />
-      </Head>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Subhasis Das|Portfolio</title>
+				<meta
+					name="description"
+					content="The official portfolio website of Subhasis Das"
+				/>
+				<link rel="icon" href="/images/favicon.ico" />
+			</Head>
+			<Header />
+			<Container fluid>{children}</Container>
+			<Footer />
+		</>
+	);
 };
 
 export default Layout;
