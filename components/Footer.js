193c3dd14f7349d531d00c76fa564832ac25bba6
subhasis020299 <subhasisdas125@gmail.com>
2022-02-15 21:49:11 +0530

    added social links icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { External } from "./Icons";
+import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
 	return (
@@ -8,30 +8,7 @@ const Footer = () => {
 				<span>Made with ❤️ by Subhasis Das</span>
 
 				<div className="container mx-auto px-4 text-secondary text-center p-4 flex justify-center items-center">
-					<a
-						href="https://github.com/subhasis020299"
-						target="_blank"
-						rel="noopener noreferrer"
-						className=" flex justify-center items-center  hover:underline hover:text-lg hover:text-highlight transition duration-150 ease-in-out"
-					>
-						<span className="mx-2">GitHub</span> <External />
-					</a>
-					<a
-						href="https://linkedin.com/in/subhasis020299"
-						target="_blank"
-						rel="noopener noreferrer"
-						className=" flex justify-center items-center  hover:underline hover:text-lg hover:text-highlight transition duration-150 ease-in-out"
-					>
-						<span className="mx-2">LinkedIn</span> <External />
-					</a>
-					<a
-						href="https://twitter.com/geram_er_chhele"
-						target="_blank"
-						rel="noopener noreferrer"
-						className=" flex justify-center items-center  hover:underline hover:text-lg hover:text-highlight transition duration-150 ease-in-out"
-					>
-						<span className="mx-2">Twitter</span> <External />
-					</a>
+					<SocialLinks />
 				</div>
 			</div>
 		</footer>
---
+++ components/SocialLinks.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const SocialLinks = () => {
+	return (
+		<>
+			<a
+				href="https://github.com/subhasis020299"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="mx-2 border-4 border-highlight rounded-full"
+			>
+				<img
+					src="/github.png"
+					alt="github"
+					height={50}
+					width={50}
+					className="bg-secondary rounded-full p-1  hover:p-2 transition-all duration-200"
+				/>
+			</a>
+			<a
+				href="https://linkedin.com/in/subhasis020299"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="mx-2 border-4 border-highlight rounded-full"
+			>
+				<img
+					src="/linkedin.png"
+					alt="linkedin"
+					height={50}
+					width={50}
+					className="bg-secondary rounded-full p-1  hover:p-2 transition-all duration-200"
+				/>
+			</a>
+			<a
+				href="https://twitter.com/geram_er_chhele"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="mx-2 border-4 border-highlight rounded-full"
+			>
+				<img
+					src="/twitter.png"
+					alt="twitter"
+					height={50}
+					width={50}
+					className="bg-secondary rounded-full p-1  hover:p-2 transition-all duration-200"
+				/>{" "}
+			</a>
+		</>
+	);
+};
+
+export default SocialLinks;
