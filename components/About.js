176e408b3b7d859a382451b0064ee1e6ac3d02e8
subhasis020299 <subhasisdas125@gmail.com>
2022-02-22 19:57:14 +0530

    overall css changes

--- components/About.js
+++ components/About.js
@@ -2,49 +2,93 @@ import React from "react";
 
 const About = () => {
 	return (
-		<div className="grid grid-cols-1 text-secondary min-h-screen">
-			<div className="flex flex-col justify-center items-center">
-				<h1 className="text-4xl text-center my-20 p-2 ">About Me</h1>
-				<div className="md:w-1/2 ">
-					<p className="text-lg text-justify my-auto px-4 py-4">
-						I am a CSE graduate from{" "}
-						<a
-							href="https://www.heritageit.edu/"
-							target="_blank"
-							rel="noreferrer"
-							className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
-						>
-							Heritage Institute of Technology, Kolkata
-						</a>
-						{", "}
-						currently working as a DevOps Engineer at{" "}
-						<a
-							href="https://tcs.com/"
-							target="_blank"
-							rel="noreferrer"
-							className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
-						>
-							Tata Consultancy Services.
-						</a>
-					</p>
-					<p className="text-lg text-justify my-auto px-4 py-4">
-						I am a passionate software engineer who loves solving real world
-						problems by writing code. I love architecting and building
-						applications that work efficiently at scale and add value to the
-						world. My primary expertise lies in fullstack development using
-						Javascript (specifically Node.js and React). I am also proficient in
-						cloud and devops engineering using AWS.
-					</p>
-					<p className="text-lg text-justify my-auto px-4 py-4">
-						These are the technologies that I regularly work with / have worked
-						with in the past -
+		<div className="flex flex-col justify-center items-center text-secondary min-h-screen">
+			<h1 className="text-4xl text-center my-20 p-2 ">About Me</h1>
+			<div className="w-full md:w-4/5">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+					<div className="px-4">
+						<img
+							src="me_with_mic.jpg"
+							alt="me"
+							width={550}
+							className="border-8 border-highlight object-fill max-h-80"
+						/>
+					</div>
+					<div className="text-lg text-justify px-4 py-4">
+						<p className="py-2">
+							I am a passionate software engineer who loves solving real world
+							problems by writing code. I love architecting and building
+							applications that work efficiently at scale and add value to the
+							world.
+						</p>
+						<p className="py-2">
+							I graduated from{" "}
+							<a
+								href="https://www.heritageit.edu/"
+								target="_blank"
+								rel="noreferrer"
+								className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
+							>
+								Heritage Institute of Technology, Kolkata
+							</a>{" "}
+							with a Bachelors in Computer Science and Engineering in 2021. I
+							was actively involved with developer communities like{" "}
+							<a
+								href="https://dschit.com/"
+								target="_blank"
+								rel="noreferrer"
+								className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
+							>
+								DSC HIT
+							</a>{" "}
+							and{" "}
+							<a
+								href="https://gdgkolkata.org/"
+								target="_blank"
+								rel="noreferrer"
+								className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
+							>
+								GDG Kolkata
+							</a>
+							, during my college days. I am currently working full-time as a
+							DevOps Engineer at{" "}
+							<a
+								href="https://tcs.com/"
+								target="_blank"
+								rel="noreferrer"
+								className="cursor-pointer hover:text-highlight hover:underline transition duration-200"
+							>
+								Tata Consultancy Services.
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-2">
+					<div className="text-lg text-justify px-4 py-4">
+						<p className="py-2">
+							My expertise lies in fullstack development using Javascript
+							(specifically Node.js, MongoDB, React) and cloud and devops
+							engineering (AWS, Docker, Kubernetes, Terraform). I have desgined,
+							developed and deployed full-scale microservices as well as
+							web-applications.
+						</p>
+						<p className="py-2">
+							I am a huge fan of serverless technologies and infrastructure
+							automation (IaC). I am also familiar with Python, Java and C++.
+						</p>
+						<p className="py-2">
+							I am currently upskilling to learn and adapt to Web3.0 with
+							technologies like Ethereum, Solidity and Web3JS
+						</p>
+					</div>
+					<div className="py-8 px-4">
 						<img
 							src="/skills.png"
-							className="border-8 border-highlight bg-secondary object-fill p-4 mx-auto my-10 w-full md:w-1/2"
+							alt="skills"
+							width={550}
+							className="border-8 border-highlight bg-secondary object-fill max-h-80"
 						/>
-						Apart from these, I am also a Web3.0 Enthusiast - exploring smart
-						contracts using solidity and JS libraries like web3js.
-					</p>
+					</div>
 				</div>
 			</div>
 		</div>
